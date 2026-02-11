c622b5b1cd674d84c437002639d299edee684510
nallupavithra <nallupavithra03@gmail.com>
2026-02-11 23:05:41 +0530

    Add files via upload
    # To-Do List Application
    
    A simple To-Do List web application built using HTML, CSS, and JavaScript.
    
    ## Features
    - Add new tasks
    - Mark tasks as completed
    - Delete tasks
    - Simple and responsive UI
    
    ## Technologies Used
    - HTML
    - CSS
    - JavaScript

---
+++ script.js
@@ -0,0 +1,27 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function () {
+        this.style.textDecoration = "line-through";
+    };
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
